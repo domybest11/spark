306b5e009755d77d2e5b7ccc9321d626fd20319c
qiuliang01 <qiuliang01@bilibili.com>
2021-03-30 21:34:15 +0800

    spark强制row_number 后有order by

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -3145,10 +3145,12 @@ class Analyzer(override val catalogManager: CatalogManager)
    */
   object ResolveWindowOrder extends Rule[LogicalPlan] {
     def apply(plan: LogicalPlan): LogicalPlan = plan resolveExpressions {
+      /**
       case WindowExpression(wf: WindowFunction, spec) if spec.orderSpec.isEmpty =>
         failAnalysis(s"Window function $wf requires window to be ordered, please add ORDER BY " +
           s"clause. For example SELECT $wf(value_expr) OVER (PARTITION BY window_partition " +
           s"ORDER BY window_ordering) from table")
+       */
       case WindowExpression(rank: RankLike, spec) if spec.resolved =>
         val order = spec.orderSpec.map(_.child)
         WindowExpression(rank.withOrder(order), spec)
